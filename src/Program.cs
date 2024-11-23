dffc14c4353b39939504b7a7f6deb22c98576f5c
Ov poddar Windows <ayanpoddar0@gmail.com>
2024-11-23 20:13:18 +0530

    adding basic requestparsher

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using src;
+using System.Net;
+using System.Runtime.CompilerServices;
+
+const int LISTENERPORT = 9546;
+
+using (var listener = new HttpListener())
+{
+    listener.Prefixes.Add($"http://localhost:{LISTENERPORT}/");
+    listener.Prefixes.Add($"http://127.0.0.1:{LISTENERPORT}/");
+    listener.Start();
+    foreach (var item in listener.Prefixes)
+        Console.WriteLine($"application is listening on {item}");
+
+    listener.BeginGetContext(ReceivedRequest, listener);
+    Console.ReadLine();
+}
+
+[SkipLocalsInit]
+void ReceivedRequest(IAsyncResult ar)
+{
+    var listener = ar.AsyncState as HttpListener;
+    if (listener == null)
+        return;
+    var context = listener.EndGetContext(ar);
+    var requestLength = (int)context.Request.ContentLength64;
+    Span<byte> requestContext = stackalloc byte[requestLength < 1024 ? requestLength : 1024];
+
+    if (RequestParser.TryProcessForBlockChainNetworks(context.Request, requestContext))
+    {
+
+        return;
+    }
+
+    context.Response.OutputStream.Write("hello World! it's listening"u8);
+    context.Response.OutputStream.Close();
+    listener.BeginGetContext(ReceivedRequest, listener);
+}
---
+++ src/RequestParser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src;
+internal static class RequestParser
+{
+    const string METHOD = "POST";
+    const string ALLOWEDCONTENTTYPE = "application/json";
+    const string TARGETEDPATH = "/";
+
+    internal static bool TryProcessForBlockChainNetworks(HttpListenerRequest request, Span<byte> requestContext)
+    {
+        if (!string.Equals(request.HttpMethod, METHOD, StringComparison.OrdinalIgnoreCase)
+            || !string.Equals(request.ContentType, ALLOWEDCONTENTTYPE, StringComparison.OrdinalIgnoreCase)
+            || request.Url == null
+            || !string.Equals(request.Url.AbsolutePath, TARGETEDPATH, StringComparison.OrdinalIgnoreCase)
+            || !request.IsWebSocketRequest)
+            return false;
+
+        request.InputStream.ReadExactly(requestContext);
+        return true;
+
+    }
+}
