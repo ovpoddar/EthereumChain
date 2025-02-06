114edcd19714d276cf1fbe832dfaf454c8fc9931
Ov poddar Windows <ayanpoddar0@gmail.com>
2025-02-06 23:03:28 +0530

    need a better way to handle the reader

---
+++ src/API/Models/SharedBufferContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.InteropServices;
+
+namespace API.Models;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct SharedBufferContext
+{
+    // 0 - safe to write, 1 - safe to read
+    public byte State;
+    public int Size;
+    // 0 - false, 1 - true
+    public byte IsContinues;
+}
--- src/API/Processors/Communication/DataReceivedMemeoryProcesser.cs
+++ src/API/Processors/Communication/DataReceivedMemeoryProcesser.cs
@@ -1,4 +1,5 @@
 ﻿using API.Exceptions;
+using API.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -26,9 +27,10 @@ internal class DataReceivedMemoryProcessor : IDisposable, ICommunication
     private readonly int _writeableSize;
     private readonly Action<byte[]> _action;
 
+    // TODO:FIX multipal reader causing deadlock
     public unsafe DataReceivedMemoryProcessor(string name, bool isNameCreator, Action<byte[]> action)
     {
-        _writeableSize = Setting.SharedMemorySize - sizeof(byte) - sizeof(int);
+        _writeableSize = Setting.SharedMemorySize - Marshal.SizeOf<SharedBuffer>();
         Name = name;
         if (isNameCreator)
         {
@@ -80,94 +82,67 @@ internal class DataReceivedMemoryProcessor : IDisposable, ICommunication
         }
     }
 
-    // check the index access
     public unsafe void SendData(byte[] data)
     {
-        var context = (byte*)_pointer.ToPointer();
-        if (data.Length > _writeableSize)
+        var context = (SharedBuffer*)_pointer.ToPointer();
+        var totalWritten = 0;
+        var isContextNotWritten = true;
+
+        while (true)
         {
-            var totalWritten = 0;
-            while (totalWritten != data.Length)
+            if (IsDisposed || totalWritten == data.Length)
+                break;
+            if (context->State != 0)
+                continue;
+            if (isContextNotWritten)
             {
-                if (context[0] == 0)
-                {
-                    Marshal.Copy(data,
-                        totalWritten,
-                        _pointer + sizeof(int) + sizeof(byte),
-                        Math.Min(data.Length - totalWritten, _writeableSize));
-                    context[0] = 1;
-                }
-                Task.Delay(100).Wait();
+                context->IsContinues = (byte)(data.Length > _writeableSize ? 1 : 0);
+                context->Size = data.Length;
+                isContextNotWritten = false;
             }
-            context[0] = 1;
-        }
-        else
-        {
-            while (true)
-            {
-                if (IsDisposed || context[0] == 0)
-                {
-                    Marshal.Copy(data,
-                        0,
-                        _pointer + sizeof(int) + sizeof(byte),
-                        data.Length);
-                    context[0] = 1;
-                    break;
-                }
-            }
+
+            var writtenLength = Math.Min(data.Length - totalWritten, _writeableSize);
+            Marshal.Copy(data,
+                totalWritten,
+                _pointer + Marshal.SizeOf<SharedBuffer>(),
+                writtenLength);
+            totalWritten += writtenLength;
+            context->State = 1;
         }
     }
 
     public unsafe void StartWorker()
     {
-        var context = (byte*)_pointer.ToPointer();
-        Debug.Assert(_action != null);
+        var context = (SharedBuffer*)_pointer.ToPointer();
+        var read = 0;
+        byte[] data = [];
         while (true)
         {
             if (IsDisposed)
                 break;
-            if (context[0] != 1)
+            if (context->State != 1)
                 continue;
 
-
             Thread.MemoryBarrier();
-            var data = new byte[Setting.SharedMemorySize];
-            Marshal.Copy(_pointer + sizeof(int) + sizeof(byte),
-                data, 
-                0,
-                Setting.SharedMemorySize);
-            _action.Invoke(data);
-            context[0] = 0;
+
+            if (read == 0)
+                data = new byte[context->Size];
+
+            var readSize = Math.Min(context->Size - read, _writeableSize);
+
+            Marshal.Copy(_pointer + Marshal.SizeOf<SharedBuffer>(),
+                   data,
+                   read,
+                   readSize);
+            read += readSize;
+
+            if (context->IsContinues == 0 || context->Size == read)
+            {
+                read = 0;
+                _action!.Invoke(data);
+            }
+            context->State = 0;
         }
     }
+
 }
-
-
-/*******************************************************************************************
-Sender class:
-    has a method which takes the data.
-
-after creating the class it will establish the communication channel
-
-after calling the sender method it will do this
-
-the full Shared buffer
-***************************
-^
-set this value to 1, and print the message for remaining byte
-state value can be like 
-0. safe to write
-1. safe to read
-
-Receiver class:
-    has a method which raised the event when the data is received.
-
-after creating the class it will establish the communication channel
-
-after binding the callback. it will run a continuous loop to check the state of the buffer.
-
-***************************
-^
-checking this value if it is 1 then read the data and raise the event
-and also have to set the value to 0 after reading the data
-********************************************************************************************/
